a93d1b76539fbbbd08823b107f0d371c98da215e
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-08-25 19:57:18 -0300

    Refactory Part 7 - Fix error by one (because i did the initial shiny-golden-bag twice) and clean up

--- src/advent-of-code-2020/day-07/handy-haversacks.clj
+++ src/advent-of-code-2020/day-07/handy-haversacks.clj
@@ -7,18 +7,12 @@
 (def input (slurp "./src/advent-of-code-2020/day-07/day-07.txt"))
 
 (defn to-key [text]
-  (-> (s/trim text)
-      (s/replace "bags" "bag")
-      (s/replace " " "-")
-      (keyword)))
+  (-> (s/trim text) (s/replace "bags" "bag") (s/replace " " "-") (keyword)))
 
 (to-key "light red bags ")
 
 (defn to-vals [text]
-  (->> (-> (s/trim text)
-           (s/replace "bags" "bag")
-           (s/replace "." "")
-           (s/split #", "))
+  (->> (-> (s/trim text) (s/replace "bags" "bag") (s/replace "." "") (s/split #", "))
        (map (fn [x]
               (if (= x "no other bag") [:no-other-bag 0]
                   (let [num (re-find #"\d+" x)]
@@ -30,41 +24,34 @@
 (defn rule-set [text]
   (->> (s/split text #"\n")
        (map #(s/split % #"contain"))
-       (map (fn [[key val]]
-              [(to-key key) (to-vals val)]))
+       (map (fn [[key val]] [(to-key key) (to-vals val)]))
        (into {})))
 
 (def bags-ex (rule-set example))
 (def bags-ex2 (rule-set example2))
 (def bags (rule-set input))
 
-(defn tree-v [k visited bags]
-  (let [k (if (keyword? k) k (first k))]
-    (map #(cond
-            (visited %) false
-            (= % :no-other-bag) false
-            (= % :shiny-gold-bag) true
-            :else (tree-v % (conj visited %) bags))
-         (keys (bags k)))))
+(defn contains-bag-color? [color bag bags]
+  (some (fn [[sub-bag-color _]] (or (= color sub-bag-color)
+                                    (contains-bag-color? color sub-bag-color bags)))
+        (bag bags)))
 
-(defn bags-with-shiny-gold [bags] (->> (map #(tree-v  % #{} bags) (keys bags))
-                                       (map flatten)
-                                       (map #(not-every? false? %))
-                                       (filter true?)
-                                       (count)))
+(defn bags-with-shiny-gold [bags]
+  (->> (keys bags)
+       (filter #(contains-bag-color? :shiny-gold-bag % bags))
+       (count)))
 
+;; part 1
 (= 4 (bags-with-shiny-gold bags-ex))
 (= 115 (bags-with-shiny-gold bags))
 
-(defn tree-vd [bags [start n]]
-  (if-let [nexts (bags start)]
-    (+ n (reduce #(+ %1 (* n (tree-vd bags %2))) 0 nexts))
-    0))
+(defn count-bag-in-bags [curr-bag bags]
+  (->> (curr-bag bags)
+       (reduce (fn [acc [next-bag n]]
+                 (+ acc n
+                    (* n (count-bag-in-bags next-bag bags)))) 0)))
 
-(defn how-many-bags-inside [bag bags]
-  (dec (tree-vd bags [bag 1])))
-
-;; why is it wrong by one ???
-(= 32 (how-many-bags-inside :shiny-gold-bag bags-ex))
-(= 126 (how-many-bags-inside :shiny-gold-bag bags-ex2))
-(= 1250 (how-many-bags-inside :shiny-gold-bag bags))
+;; part 2
+(= 32 (count-bag-in-bags :shiny-gold-bag bags-ex))
+(= 126 (count-bag-in-bags :shiny-gold-bag bags-ex2))
+(= 1250 (count-bag-in-bags :shiny-gold-bag bags))
