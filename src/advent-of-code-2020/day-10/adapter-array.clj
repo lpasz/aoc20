3c401bb69f4a77e32f82dda85fc48df678cb26fb
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-08-27 09:49:50 -0300

    Half Working Version

--- src/advent-of-code-2020/day-10/adapter-array.clj
+++ src/advent-of-code-2020/day-10/adapter-array.clj
@@ -25,9 +25,43 @@
        (frequencies)))
 
 (jolt-jumps 0 ex)
-(jolt-jumps 0 ex2) 
+(jolt-jumps 0 ex2)
 (let [r (jolt-jumps 0 inp)]
   (* (r 1) (r 3)))
+
+(defn jolt-all-jumps [source-jolt adapter-text]
+  (->> (to-int-seq adapter-text)
+       (jolt-source source-jolt)
+       (jolt-charge-receiver)
+       (sort)))
+
+(let [s (jolt-all-jumps 0 ex)]
+  (->> (for [s1 s
+             s2 s
+             :when (and (< s1 s2) (<= (- s2 s1) 3))]
+         [s1 s2])
+       (reduce (fn [acc [k v]]
+                 (if (acc k)
+                   (update acc k conj v)
+                   (assoc acc k [v])))
+               (sorted-map))))
+
+
+(def m (sort-by first (map (fn [[k v]] [k v]) {0 [1], 1 [4], 4 [5 6 7], 5 [6 7], 6 [7], 7 [10], 10 [11 12], 11 [12], 12 [15], 15 [16], 16 [19], 19 [22]})))
+m
+(defn func [next-n nodes]
+  [0] [1] [4] [5 6 7])
+
+(reduce (fn [acc [k vs]]
+          ;; (mapcat (fn [a]
+          ;;           (if (= (last a) k)
+          ;;             (map #(conj a %) vs)
+          ;;             a)) acc))
+          (reduce (fn [acc2 a]
+                    (if (= (last a) k)
+                      (into [] (concat acc2 (map #(conj a %) vs)))
+                      (conj acc2 a))) [] acc)) 
+        #{[0]} m)
 
 
 
