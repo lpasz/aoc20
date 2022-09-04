8f7fa0de80b800f88d2ef5dc405b53ffb762d5de
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-09-04 16:10:46 -0300

    Day 15 Performance Boost

--- src/advent-of-code-2020/day-15/rambunctious-recitation.clj
+++ src/advent-of-code-2020/day-15/rambunctious-recitation.clj
@@ -1,28 +1,26 @@
-(ns advent-of-code-2020.day-15.rambunctious-recitation
-  (:require [clojure.string :as str]))
+(ns advent-of-code-2020.day-15.rambunctious-recitation)
 
-(defn take-turn [acc start-nums prev now-t stop-at-t]
+(defn turn-say [prev-ts]
+  (if (<= 2 (count prev-ts))
+    (- (last prev-ts) (first prev-ts))
+    0))
+
+(defn turn-say-ts [say-ts now-t]
+  (if-let [say-t (last say-ts)]
+    [say-t now-t]
+    [now-t]))
+
+(defn take-turn [acc start-nums [prev prev-ts] now-t stop-at-t]
   (cond
     (> now-t stop-at-t) prev
-    (empty? start-nums) (let [prev-ts (acc prev)
-                              [t1 t2] (take-last 2 prev-ts)
-                              [acc say] (if (<= 2 (count prev-ts))
-                                          [(assoc acc prev [t1 t2]) (- t2 t1)]
-                                          [acc 0])]
-                          (recur (assoc acc say (conj (or (acc say) []) now-t))
-                                 []
-                                 say
-                                 (inc now-t)
-                                 stop-at-t))
+    (empty? start-nums) (let [say (turn-say prev-ts)
+                              say-ts (turn-say-ts (acc say) now-t)]
+                          (recur (assoc acc say say-ts) [] [say say-ts] (inc now-t) stop-at-t))
     :else (let [say (first start-nums)]
-            (recur (assoc acc say [now-t])
-                   (rest start-nums)
-                   say
-                   (inc now-t)
-                   stop-at-t))))
+            (recur (assoc acc say [now-t]) (rest start-nums) [say [now-t]] (inc now-t) stop-at-t))))
 
 (defn will-be-said-in-nth [star-nums nth]
-  (take-turn (sorted-map) star-nums nil 1 nth))
+  (take-turn {} star-nums [] 1 nth))
 
 (def ex0 [0 3 6]) ;; 436
 (def ex1 [1 3 2]) ;; 1
@@ -34,8 +32,8 @@
 (def inp [12 1 16 3 11 0]) ;; 1696
 
 ;;part 1
-
-(will-be-said-in-nth ex0 2020)
+(time (will-be-said-in-nth ex0 2020))
+(will-be-said-in-nth ex0 10)
 (will-be-said-in-nth ex1 2020)
 (will-be-said-in-nth ex2 2020)
 (will-be-said-in-nth ex3 2020)
@@ -45,6 +43,5 @@
 (will-be-said-in-nth inp 2020)
 
 ;; part 2
-
-(will-be-said-in-nth ex0 30000000) ;; 175594
+(time (will-be-said-in-nth ex0 30000000)) ;; 175594
 (will-be-said-in-nth inp 30000000) ;; 37385
