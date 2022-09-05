0a6ee43cb0bf3c51fe20b26b7b9381cfdb8e9bc4
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-09-05 17:51:01 -0300

    Day 17 - Part 1 and Part 2 (WTF)

--- src/advent-of-code-2020/day-17/conway-cubes.clj
+++ src/advent-of-code-2020/day-17/conway-cubes.clj
@@ -7,33 +7,35 @@
 (defn distance [x]
   (range (dec x) (+ 2 x)))
 
-(distance 1)
+(defn cartesian-prod [colls]
+  (if (empty? colls)
+    '(())
+    (for [more (cartesian-prod (rest colls))
+          x (first colls)]
+      (cons x more))))
 
-(defn neighbors-coord [[x y z]]
-  (for [i (distance x)
-        j (distance y)
-        k (distance z)
-        :when (not= [x y z] [i j k])]
-    [i j k]))
+(defn neighbors-coord [coord]
+  (->> coord
+       (map distance)
+       (cartesian-prod)
+       (filter #(not= % coord))))
 
-(neighbors-coord [0 0 0])
 
-(defn str->pocket-dimention [text]
-  (->> (s/split text #"\n")
+(defn pocket-dimention [text n-dimentions]
+  (->> (s/split-lines text)
        (keep-indexed (fn [y line]
                        (->> line
-                            (keep-indexed (fn [x itm] (when (= \# itm) [[x y 0] true]))))))
-       (reduce (fn [acc states] (->>
-                                 states
-                                 (reduce (fn [acc [coord state]]
-                                           (assoc acc coord state)) acc)))
-               {})))
+                            (keep-indexed (fn [x itm] (when (= \# itm) (concat [x y] (repeat (- n-dimentions 2) 0))))))))
+       (flatten)
+       (partition n-dimentions)
+       (set)))
+
+(pocket-dimention ex1 4)
 
 (defn count-atives [coord dimention]
   (->> coord
        (neighbors-coord)
-       (map dimention)
-       (filter true?)
+       (filter dimention)
        (count)))
 
 (defn remain-active? [coord dimention]
@@ -44,39 +46,36 @@
 
 (defn new-state [coord dimention]
   (if (dimention coord)
-    (remain-active? coord dimention)
-    (activate? coord dimention)))
-
-
-(->> (str->pocket-dimention ex1)
-     (new-state [0 0 0]))
+    (when (remain-active? coord dimention) coord)
+    (when (activate? coord dimention) coord)))
 
 (defn new-dimention [dimention]
   (->> dimention
-       (keys)
        (mapcat neighbors-coord)
        (into #{})))
-
-(new-dimention (str->pocket-dimention ex1))
 
 (defn do-cycle [dimention]
   (->> dimention
        (new-dimention)
        (reduce (fn [acc coord]
                  (if (new-state coord dimention)
-                   (assoc acc coord true)
+                   (conj acc coord)
                    acc))
-               (sorted-map))))
+               #{})))
 
 (defn after-n-cycles [n dimention]
   (reduce (fn [dimention _] (do-cycle dimention)) dimention (range n)))
 
-(defn active-after-n-cycles [n text]
-  (->> text
-       (str->pocket-dimention)
+(defn active-after-n-cycles [n text n-dim]
+  (->> (pocket-dimention text n-dim)
        (after-n-cycles n)
        (filter second)
        (count)))
 
-(active-after-n-cycles 6 ex1) ;; 112
-(active-after-n-cycles 6 inp) ;; 218
+;; part 1
+(active-after-n-cycles 6 ex1 3) ;; 112
+(active-after-n-cycles 6 inp 3) ;; 218
+
+;; part 2
+(active-after-n-cycles 6 ex1 4) ;; 848
+(active-after-n-cycles 6 inp 4) ;; 1908
